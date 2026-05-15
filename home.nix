4b6fbcf74831b3e622cd14a1b007ac3223ba2d8b
Bashbreakr <samuel.ppp@gmx.de>
2026-05-15 15:28:14 +0200

    home-manager backups

--- home.nix
+++ home.nix
@@ -17,6 +17,8 @@
   home.username = "bashbreakr";
   home.homeDirectory = "/home/bashbreakr";
 
+  home-manager.backupFileExtension = "backup";
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
